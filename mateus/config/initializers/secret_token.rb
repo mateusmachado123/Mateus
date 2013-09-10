7e1549e4c7534599ddd0d68494fb873f3c319226
OpenShift guest <mateus.machado@unerj.br>
2013-09-09 20:44:08 -0400

    commit inicial

---
+++ mateus/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mateus::Application.config.secret_token = '0212f634365b061631be0587e2823fec14d36cb950e0c57e76c46860dbcdf33951e96fe58303fde7f3ae41fe14501421b7ab2ae9b67ffebb10523323a3599174'
